ecdaaa24617f605d85aaec3f14bf51f2f3317eb3
Piebee007 <tom.feilden@btinternet.com>
2023-07-30 12:02:56 +0100

    New Recipe

---
+++ Scripts/GetDesserts.py
@@ -0,0 +1,71 @@
+import glob
+
+def total_time(prep_time, cook_time):
+    total_time = 0
+    prep_time = prep_time.split(":")
+    total_time += int(prep_time[0]) *60 + int(prep_time[1])
+    cook_time = cook_time.split(":")
+    total_time += int(cook_time[0]) *60 + int(cook_time[1])
+    
+    return str(total_time)
+
+unique_categories = []
+
+file_text = ""
+file_array = glob.glob('Recipes/*.txt')
+for file_name in file_array:
+    recipe_data = ""
+    #The name of the recipe
+    #recipe_data += file_name[8:len(file_name)-4] + ", "
+    recipe_name = file_name[8:len(file_name)-4] + ", "
+    is_dessert = False
+    file = open(file_name, "r")
+    for line in file:
+        if "Category: Dessert" in line:
+            is_dessert = True
+            category = line[10:len(line)-1]
+            if category != "/n":
+                recipe_data += recipe_name + category + ", "
+            else:
+                recipe_data += "ERROR" + ", "
+            if category not in unique_categories:
+                if category != "/n":
+                    unique_categories.append(category)
+                else:
+                    unique_categories.append("Error")
+        if is_dessert == True:
+            if line[0:11] == "Prep Time: ":
+                if line[11:len(line)] != "/n":
+                    prep_time = line[11:len(line)-1]
+                    recipe_data += prep_time + ", "
+                else:
+                    recipe_data += "ERROR" + ", "
+            if line[0:11] == "Cook Time: ":
+                if line[11:len(line)] != "/n":
+                    cook_time = line[11:len(line)-1]
+                    recipe_data += cook_time + ", "
+                else:
+                    recipe_data += "ERROR, " 
+
+                recipe_data += total_time(prep_time,cook_time)
+        
+    if is_dessert == True:   
+        file_text += recipe_data + "\n"
+    file.close()
+
+
+print(unique_categories)
+sorted_unique_categories = sorted(unique_categories)
+unique_tags_text = ""
+for tag in sorted_unique_categories:
+    unique_tags_text += ", " +tag 
+unique_tags_text = unique_tags_text[2:len(unique_tags_text)]
+unique_tags_text += "\n"
+
+print(file_text)
+file = open("Desserts.txt", "w")
+file.write(unique_tags_text)
+file.write(file_text)
+file.close()
+
+
